0b3431e2cf35b9d3f0d0e89ce78c42374d2ef2ed
Daniel Ritz <daniel.ritz@gmx.ch>
2015-04-04 00:17:06 +0200

    Add simplecov to see test coverage
    73% with Redmine 3.0.1

---
+++ Gemfile
@@ -0,0 +1,5 @@
+group :test do
+  if !dependencies.any? { |d| d.name == "simplecov" }
+    gem "simplecov", :require => false
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,19 @@
-helper_path = '/../../test/test_helper'
+require 'simplecov'
 
-helper = File.expand_path(File.dirname(__FILE__) + helper_path)
-
-# Rails 2
-if !File.exists?(helper)
-  helper = File.expand_path(File.dirname(__FILE__) + "/.." + helper_path)
+if Dir.pwd.match(/plugins\/redmine_stealth/)
+  covdir = 'coverage'
+else
+  covdir = 'plugins/redmine_stealth/coverage'
 end
 
-require helper
+SimpleCov.coverage_dir(covdir)
+SimpleCov.start 'rails' do
+  add_filter do |source_file|
+    # only show files belonging to the plugin, except init.rb which is not fully testable
+    source_file.filename.match(/redmine_stealth/) == nil ||
+      source_file.filename.match(/redmine_stealth\/init.rb/) != nil
+  end
+end
+
+# Load the Redmine helper
+require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
